de001936190bf8ecf4df9af991fa78e21a764406
minhanhiscoolish <minhanh3mc@gmail.com>
2025-12-17 13:57:17 +0700

    first commit

---
+++ include/common.h
@@ -0,0 +1,14 @@
+#include "asio.hpp"
+#include <algorithm>
+#include <atomic>
+#include <chrono>
+#include <condition_variable>
+#include <functional>
+#include <iostream>
+#include <memory>
+#include <mutex>
+#include <string>
+#include <thread>
+#include <unordered_map>
+#include <utility>
+#include <vector>
---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+#include "connx/handshake.cpp"
+#include "include/common.h"
+
+int main(int argc, char **argv) {
+  std::cout << "asd";
+  return 0;
+}
