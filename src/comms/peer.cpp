080f1e7e3f6ee738f34b4e22f7a77b18865793a9
minhanhiscoolish <minhanh3mc@gmail.com>
2026-01-11 23:56:33 +0800

    use after move bug

--- src/comms/peer.cpp
+++ src/comms/peer.cpp
@@ -39,8 +39,8 @@ void ConnectionPeer::do_accept() {
 void ConnectionPeer::handle_new_peer(tcp::socket socket) {
     auto peer = std::make_shared<Peer>(io_context_, shared_from_this());
 
-    peer->socket = std::move(socket);
     peer->endpoint = socket.remote_endpoint();
+    peer->socket = std::move(socket);
     peer->state = epsp_state_peer_t::EPSP_STATE_PEER_DISCONNECTED;
 
     peers_pending_.emplace(std::move(peer));
@@ -50,14 +50,12 @@ void ConnectionPeer::start(const uint32_t &target_id,
                            const asio::ip::tcp::endpoint &endpoint) {
     auto self(shared_from_this());
     auto peer = std::make_shared<Peer>(io_context_, shared_from_this());
-    auto *raw = peer.get();
 
     peer->endpoint = endpoint;
     peer->state = epsp_state_peer_t::EPSP_STATE_PEER_WAIT_PID_RQST;
-    peers_.emplace(target_id, std::move(peer));
 
     asio::async_connect(
-        raw->socket, std::array<tcp::endpoint, 1>{endpoint},
+        peer->socket, std::array<tcp::endpoint, 1>{endpoint},
         [self](asio::error_code ecode, const tcp::endpoint &) -> void {
             if (ecode) {
                 self->peer_logger_->error("Connect error: {}", ecode.message());
