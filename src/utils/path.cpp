4e501b3e425be292fcafa3bf7f7f19720144f7d8
minhanhiscoolish <minhanh3mc@gmail.com>
2026-04-11 13:13:58 +0800

    fixed various other bugs

--- src/utils/path.cpp
+++ src/utils/path.cpp
@@ -10,24 +10,46 @@
 
 auto get_executable_dir() -> std::filesystem::path {
     std::string path;
+
 #if defined(_WIN32)
-    std::wstring wpath(MAX_PATH, '\0');
-    DWORD size = GetModuleFileNameW(nullptr, wpath.data(), wpath.size());
-    wpath.resize(size);
-    path = std::string(wpath.begin(), wpath.end());
+
+    std::string buffer(MAX_PATH, '\0');
+    DWORD size = GetModuleFileNameA(nullptr, buffer.data(),
+                                    static_cast<DWORD>(buffer.size()));
+
+    if (size == 0) {
+        return {};
+    }
+
+    buffer.resize(size);
+    path = buffer;
 
 #elif defined(__APPLE__)
+
     uint32_t size = 0;
     _NSGetExecutablePath(nullptr, &size);
+
     std::string buffer(size, '\0');
-    _NSGetExecutablePath(buffer.data(), &size);
+
+    if (_NSGetExecutablePath(buffer.data(), &size) != 0) {
+        return {};
+    }
+
     path = buffer;
 
 #elif defined(__linux__)
-    std::string buffer(1024, '\0');
+
+    std::string buffer(PATH_MAX, '\0');
+
     ssize_t count = readlink("/proc/self/exe", buffer.data(), buffer.size());
-    buffer.resize(count);
+
+    if (count == -1) {
+        return {};
+    }
+
+    buffer.resize(static_cast<size_t>(count));
     path = buffer;
+
 #endif
 
     return std::filesystem::path(path).parent_path();
